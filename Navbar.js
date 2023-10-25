c8c9d06e09bad35447975f197d8ca34cc679af5e
tamana007 <tamana.efatwira1@gmail.com>
2023-10-25 13:11:56 -0700

    strt

--- Navbar.js
+++ Navbar.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
+import { useGlobalContext } from './context'
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
+  const {isSidebarOpen,isSubmenu,openSidebar,closeSidebar,openSubmenu,closeSubmenu}=useGlobalContext()
+  return <>
+  <nav>
+    <div className='nav-center'>
+    <div className='nav-header'>
+      <img src={logo} alt='strip' className='nav-logo'></img>
+      <button className='btn toggle-btn' onClick={openSidebar}><FaBars/></button>
+    </div>
+
+    </div>
+  </nav>
+  
+  
+  </>
 }
 
 export default Navbar
--- context.js
+++ context.js
@@ -1,2 +1,40 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext, Children } from "react";
+import sublinks from "./data";
+
+export const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenu, setIsSubmenuOpen] = useState(true);
+
+  function openSidebar() {
+    setIsSidebarOpen(true);
+  }
+  function closeSidebar() {
+    setIsSidebarOpen(false);
+  }
+  //for subMenu
+  function openSubmenu() {
+    setIsSubmenuOpen(true);
+  }
+  function closeSubmenu() {
+    setIsSubmenuOpen(false);
+  }
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isSubmenu,
+        openSidebar,
+        closeSidebar,
+        openSubmenu,
+        closeSubmenu,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
